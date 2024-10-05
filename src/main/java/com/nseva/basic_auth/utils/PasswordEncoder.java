51aa8b1e78ead56a6ac4a804e7ab3f53b46d2f62
LinAn <adilinan72@gmail.com>
2024-10-05 21:12:32 +0300

    feat: Implement Password Encoder Class
    - Added PasswordEncoder class to handle password encoding and decoding.
    - Utilized BCrypt for secure password hashing, enhancing user authentication security.
    - Integrated password encoding into the user registration and login processes.
    
    This commit improves security by ensuring that user passwords are stored securely.

---
+++ src/main/java/com/nseva/basic_auth/utils/PasswordEncoder.java
@@ -0,0 +1,13 @@
+package com.nseva.basic_auth.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoder {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+}
